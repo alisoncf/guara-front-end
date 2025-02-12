e9b578c78c769ed3d58599cd6004c1a9971265c8
alison <alison.filgueiras@ueg.br>
2025-02-12 14:39:33 -0300

    alterações

--- guara/src/pages/objetos/GerenciarMidias.vue
+++ guara/src/pages/objetos/GerenciarMidias.vue
@@ -1,26 +1,32 @@
 <script setup lang="ts">
-import { ref, onMounted, onBeforeMount } from 'vue';
-import axios from 'axios';
-import { useRouter } from 'vue-router';
-import { useDadosObjetoFisico } from '../../stores/objeto-fisico';
-import { ObjetoFisico } from './manter-objeto';
-import apiConfig from 'src/apiConfig';
-import { Notify } from 'quasar';
+import { ref, onMounted, onBeforeMount } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import { useDadosObjetoFisico } from "../../stores/objeto-fisico";
+import { ObjetoFisico } from "./manter-objeto";
+import apiConfig from "src/apiConfig";
+import { Dialog, Notify } from "quasar";
 
-const objetoId = ref({} as string ); // Ajuste conforme necessário
+const objetoId = ref({} as string); // Ajuste conforme necessário
 const objetoStore = useDadosObjetoFisico();
 
+interface Midia {
+  file: string;
+  url: string;
+  uri: string;
+}
+
 const objetoSelecionado = ref({} as ObjetoFisico);
-const midias = ref([]);
+const midias = ref([] as Midia[]);
 const midiasEncontradas = ref([]);
 const useFileUpload = ref([]);
 const thumbnails = ref([]);
 const router = useRouter();
 
 function adicionarMidia() {
-  midias.value.push({ file: null, url: '' });
-  useFileUpload.value.push(false);
-  thumbnails.value.push(null);
+  midias.value.push({ file: "", url: "", uri: "" });
+  useFileUpload.value.push();
+  thumbnails.value.push();
 }
 
 function removerMidia(index) {
@@ -44,23 +50,57 @@ function handleFileUpload(event, index) {
     };
     reader.readAsDataURL(file);
   } else {
-    console.error('Nenhum arquivo selecionado.');
+    console.error("Nenhum arquivo selecionado.");
   }
 }
 function isPDF(url) {
-  return url.endsWith('.pdf') || url.startsWith('data:application/pdf');
+  return url.endsWith(".pdf") || url.startsWith("data:application/pdf");
 }
 
 function handleToggleChange(index) {
   if (useFileUpload.value[index]) {
-    midias.value[index].url = '';
+    midias.value[index].url = "";
     thumbnails.value[index] = null;
   } else {
     midias.value[index].file = null;
     thumbnails.value[index] = null;
   }
 }
+function excluir(arquivo: string ) {
 
+  Dialog.create({
+    title: "Exclusão",
+    message: "Tem certeza que deseja excluir essa mídia e sua relação com o objeto?",
+    cancel: true,
+    persistent: true,
+  })
+    .onOk(() => {
+      axios
+        .post(apiConfig.baseURL + apiConfig.endpoints.remove_file, {
+        objetoId: objetoId.value,
+        repositorio: objetoSelecionado.value.repositorio,
+        file: arquivo,
+      })
+        .then((response) => {
+          Notify.create({
+            type: "positives",
+            message: "arquivos enviados ",
+            timeout: 5000,
+          });
+          buscarMidias();
+        })
+        .catch((error) => {
+          Notify.create({
+            type: "negative",
+            message: "erro ao salvar midia " + error,
+            timeout: 5000,
+          });
+        });
+    })
+    .onCancel(() => {
+      console.log("Usuário cancelou a saída");
+    });
+}
 function handleUrlInput(index) {
   // Validação simples para verificar se a URL é uma imagem
   if (isImage(midias.value[index].url)) {
@@ -71,7 +111,7 @@ function handleUrlInput(index) {
 }
 
 function isImage(url) {
-  return url.startsWith('data:image/') && url.includes('base64');
+  return url.startsWith("data:image/") && url.includes("base64");
 }
 function isVideo(url: string) {
   return /\.(mp4|webm|ogg)$/i.test(url);
@@ -80,34 +120,37 @@ function submitMidias() {
   const formData = new FormData();
 
   // Adiciona o objetoId ao FormData
-  formData.append('objetoId', objetoId.value);
-  formData.append('repositorio', objetoSelecionado.value.repositorio);
-  formData.append('repository', objetoSelecionado.value.repositorio);
-  console.log('selecionado', objetoSelecionado.value.repositorio);
+  formData.append("objetoId", objetoId.value);
+  formData.append("repositorio", objetoSelecionado.value.repositorio);
+  formData.append("repository", objetoSelecionado.value.repositorio);
+  console.log("selecionado", objetoSelecionado.value.repositorio);
   // Adiciona cada mídia ao FormData
   midias.value.forEach((midia, index) => {
     if (midia.file) {
-      formData.append('midias', midia.file); // Envia o arquivo diretamente
+      formData.append("midias", midia.file); // Envia o arquivo diretamente
     } else if (midia.url) {
-      formData.append('midias', midia.url); // Envia a URL como string
+      formData.append("midias", midia.url); // Envia a URL como string
     }
   });
 
   axios
     .post(apiConfig.baseURL + apiConfig.endpoints.upload, formData, {
       headers: {
-        'Content-Type': 'multipart/form-data', // Define o cabeçalho correto
+        "Content-Type": "multipart/form-data", // Define o cabeçalho correto
       },
     })
     .then((response) => {
-      console.log('Mídias salvas com sucesso:', response.data);
-      alert('Mídias salvas com sucesso!');
+      Notify.create({
+        type: "positives",
+        message: "arquivos enviados ",
+        timeout: 5000,
+      });
       buscarMidias();
     })
     .catch((error) => {
       Notify.create({
-        type: 'negative',
-        message: 'erro ao salvar midia ' + error,
+        type: "negative",
+        message: "erro ao salvar midia " + error,
         timeout: 5000,
       });
     });
@@ -117,12 +160,11 @@ function buscarMidias() {
     .get(`${apiConfig.baseURL}${apiConfig.endpoints.listar_arquivos}`, {
       params: {
         objetoId: objetoId.value,
-        repositorio: objetoSelecionado.value.repositorio
-      }
+        repositorio: objetoSelecionado.value.repositorio,
+      },
     })
     .then((response) => {
-
-      console.log('Resposta da API:', response.data);
+      console.log("Resposta da API:", response.data);
 
       const midiasLocais = response.data.arquivos_locais || [];
       const midiasSparql = response.data.arquivos_sparql?.results?.bindings || [];
@@ -130,27 +172,27 @@ function buscarMidias() {
       // Processar arquivos locais
       const midiasLocaisFormatadas = midiasLocais.map((nome: string) => ({
         url: `${apiConfig.baseURL}/caminho_dos_arquivos/${nome}`, // Ajuste conforme necessário
-        nome: nome
+        nome: nome,
       }));
 
       // Processar arquivos retornados pelo SPARQL
       const midiasSparqlFormatadas = midiasSparql.map((item: any) => ({
         url: item.s.value,
-        nome: item.s.value.split('/').pop()
+        nome: item.s.value.split("/").pop(),
       }));
 
       // Armazenar as mídias encontradas separadamente
-      midiasEncontradas.value = [...midiasLocaisFormatadas, ...midiasSparqlFormatadas];
+      midiasEncontradas.value = [...midiasLocaisFormatadas];
       Notify.create({
-        type: 'positive',
-        message: 'mídias encontradas: ' ,
+        type: "positive",
+        message: "mídias encontradas: ",
         timeout: 5000,
       });
     })
     .catch((error) => {
       Notify.create({
-        type: 'negative',
-        message: 'Erro ao buscar mídias: ' + error,
+        type: "negative",
+        message: "Erro ao buscar mídias: " + error,
         timeout: 5000,
       });
     });
@@ -180,13 +222,17 @@ onBeforeMount(() => {
             :columns="[
               { name: 'nome', label: 'Nome', align: 'left', field: 'nome' },
               {
-                name: 'preview', label: 'Pré-visualização', align: 'center',
-                field: ''
+                name: 'preview',
+                label: '',
+                align: 'left',
+                field: '',
               },
               {
-                name: 'acao', label: 'Ação', align: 'right',
-                field: ''
-              }
+                name: 'acao',
+                label: '',
+                align: 'left',
+                field: '',
+              },
             ]"
             row-key="nome"
           >
@@ -194,14 +240,47 @@ onBeforeMount(() => {
               <q-tr :props="props">
                 <q-td key="nome">{{ props.row.nome }}</q-td>
                 <q-td key="preview">
-                  <img v-if="isImage(props.row.url)" :src="props.row.url" style="max-width: 100px; max-height: 100px;" />
-                  <video v-else-if="isVideo(props.row.url)" controls style="max-width: 100px; max-height: 100px;">
+                  <img
+                    v-if="isImage(props.row.url)"
+                    :src="props.row.url"
+                    style="max-width: 100px; max-height: 100px"
+                  />
+                  <video
+                    v-else-if="isVideo(props.row.url)"
+                    controls
+                    style="max-width: 100px; max-height: 100px"
+                  >
                     <source :src="props.row.url" />
                   </video>
-                  <q-btn v-else-if="isPDF(props.row.url)" icon="picture_as_pdf" flat dense :href="props.row.url" target="_blank" />
+                  <q-btn
+                    v-else-if="isPDF(props.row.url)"
+                    icon="picture_as_pdf"
+                    flat
+                    dense
+                    :href="props.row.url"
+                    target="_blank"
+                  />
                 </q-td>
                 <q-td key="acao">
-                  <q-btn label="Abrir" color="primary" :href="props.row.url" target="_blank" />
+                  <q-btn
+                    label="Abrir"
+                    color="primary"
+                    icon="panorama"
+                    :href="props.row.url"
+                    size="sm"
+                    flat
+                    target="_blank"
+                  />
+                </q-td>
+                <q-td key="acao">
+                  <q-btn
+                    label="Excluir"
+                    color="red"
+                    icon="delete"
+                    size="sm"
+                    flat
+                    @click="excluir(props.row.nome)"
+                  />
                 </q-td>
               </q-tr>
             </template>
